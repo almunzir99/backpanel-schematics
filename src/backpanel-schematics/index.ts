554b3fff510b5646630b79b5cf398bc8c717cbf9
almunzir99 <almunzir99@gmail.com>
2022-11-10 19:51:59 +0200

    finalize project for deployment

--- src/backpanel-schematics/index.ts
+++ src/backpanel-schematics/index.ts
@@ -1,32 +1,34 @@
-import { dasherize } from '@angular-devkit/core/src/utils/strings';
-import { apply, MergeStrategy, mergeWith, Rule, SchematicContext, strings, template, Tree, url } from '@angular-devkit/schematics';
+import { classify, dasherize } from '@angular-devkit/core/src/utils/strings';
+import { apply, MergeStrategy, mergeWith, move, Rule, SchematicContext, SchematicsException, strings, template, Tree, url } from '@angular-devkit/schematics';
 import { buildColumnsFormModels } from './utils/columns-builder';
 import { updateMenuList } from './utils/menu-list-modifier';
 import { updateDashboardRoutesArray } from './utils/routes-modifier';
 var pluralize = require('pluralize')
 
-
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function backpanelSchematics(_options: BackPanelOptions): Rule {
-  return (_tree: Tree, _context: SchematicContext) => {
+  if (!_options.group)
+    _options.group = "Pages";
+  if (!_options.title)
+    _options.title = classify(pluralize(_options.name));
+  return async (_tree: Tree, _context: SchematicContext) => {
     const sourceTemplate = url("./templates");
-    var cols = buildColumnsFormModels(_tree,_options.name);
-    var templateOptions = { name:_options.name,cols:cols};
+    var cols = buildColumnsFormModels(_tree, _options.name);
+    var templateOptions = { name: _options.name, cols: cols,title:_options.title, group:_options.group };
     const sourceParamterizedTemplate = apply(sourceTemplate, [
       template({
         ...templateOptions,
         ...strings, 
         normalize,
-      })
+      }),
     ]);
-    updateMenuList(_tree,_options.name,'Pages');
-    updateDashboardRoutesArray(_tree,_options.name);
-    
-    return mergeWith(sourceParamterizedTemplate,MergeStrategy.Overwrite);
+    updateMenuList(_tree, _options.name, _options.group!,_options.title!);
+    updateDashboardRoutesArray(_tree, _options.name);
+    return mergeWith(sourceParamterizedTemplate, MergeStrategy.Overwrite);
   };
 }
- 
+
 function normalize(name: string): string {
 
   return (dasherize(pluralize(name)));
--- src/backpanel-schematics/utils/menu-list-modifier.ts
+++ src/backpanel-schematics/utils/menu-list-modifier.ts
@@ -3,14 +3,14 @@ import { Tree } from "@angular-devkit/schematics";
 var pluralize = require('pluralize')
 const regex = /(const MenuList: MenuGroup\[\] = )(\[(?:.*?|\s*)*?\]\s*\}\s*\]);?/g;
 const path = './src/app/dashboard/components/menu/menu.list.ts';
-export function updateMenuList(tree: Tree, name: string, group: string) : Tree {
+export function updateMenuList(tree: Tree, name: string, group: string,title:string) : Tree {
     var buffer = tree.read(path);
     if (!buffer)
         throw 'menu list file is not available';
     var content = buffer.toString();
     var extractedContent = extractMenuList(content);
     content = content.replace(extractedContent, '@modified');
-    var result = updateMenuItem(extractedContent, name, group);
+    var result = updateMenuItem(extractedContent, name, group,title);
     content = content.replace('@modified',result);
     tree.overwrite(path,content);
     return tree;
@@ -23,10 +23,10 @@ function extractMenuList(content: string): string {
         throw 'Invalid menu list file content';
     return matches[2].toString();
 }
-function updateMenuItem(menuString: string, name: string, group: string):string {
+function updateMenuItem(menuString: string, name: string, group: string,title:string):string {
     var json = menuString.replace((/  |\r\n|\n|\r/gm), "");
     const newItem = {
-        title: pluralize(name),
+        title: title,
         icon: '',
         route: `/dashboard/${pluralize(name)}`,
         permissionName: `${classify(pluralize(name))}Permissions`
